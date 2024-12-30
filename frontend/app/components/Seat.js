905ba8763d1666816b7e1a6db77a06b85e9fd98c
Shubham Lahase <shubhamlahase@gmail.com>
2024-12-30 14:59:08 +0530

    Remove frontend code from backend branch

--- frontend/app/components/Seat.js
+++
@@ -1,7 +0,0 @@
-export default function Seat({ seatNumber, isBooked }) {
-  const seatStyles = `w-10 h-10 flex items-center justify-center rounded text-white font-bold text-sm ${
-    isBooked ? "bg-yellow-500" : "bg-green-500"
-  }`;
-
-  return <div className={seatStyles}>{seatNumber}</div>;
-}
